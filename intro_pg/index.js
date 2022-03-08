0b971dac5f74787e052dac9d2eb8df3da8f92733
Matias Bensan A <mbensan@gmail.com>
2022-03-08 10:59:43 -0300

    Ejemplo de uso del Client en Node-PG

---
+++ intro_pg/index.js
@@ -0,0 +1,61 @@
+// import { Client } from 'pg'
+const { Client } = require('pg')
+
+// Conexion usando un String
+//const client = new Client('postgresql://postgres:1005@localhost:5432/jeans')
+
+// conexio usando un objeto
+const client = new Client({
+  user: 'postgres',
+  password: '1005',
+  host: 'localhost',
+  port: '5432',
+  database: 'jeans'
+})
+
+client.connect(err => {
+  if (err) {
+    console.log('Error en la conexión a Postgres', err)
+  }
+})
+
+async function leer () {
+  
+  const { rows } = await client.query(
+    "select * from ropa"
+  )
+  console.log(rows);
+  client.end()
+}
+
+async function insertar (nombre, talla, color) {
+
+  const { rows } = await client.query(
+    `insert into ropa (nombre, talla, color) values ('${nombre}', '${talla}', '${color}') returning *`
+  )
+  console.log(rows);
+  client.end()
+
+}
+
+function init () {
+  const accion = process.argv[2]
+
+  if (accion == 'leer') {
+
+    leer()
+
+  } else if (accion == 'insertar') {
+
+    const nombre = process.argv[3]
+    const talla = process.argv[4]
+    const color = process.argv[5]
+
+    insertar(nombre, talla, color)
+
+  } else {
+    console.log('Esta acción no existe.');
+  }
+}
+init()
+
