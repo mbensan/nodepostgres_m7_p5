7ba0c7d27d920025d31e1e874a268b3ec73da462
Matias Bensan A <mbensan@gmail.com>
2022-03-16 10:12:43 -0300

    Ejemplo de manejo de errores (elecciones)

--- elecciones/db.js
+++ elecciones/db.js
@@ -33,6 +33,20 @@ async function get_candidatos() {
   return rows
 }
 
+async function get_historial() {
+  const client = await pool.connect()
+
+  const { rows } = await client.query({
+    text: `select historial.estado, historial.votos, candidatos.nombre
+           from historial join candidatos on candidatos.id=historial.ganador_id`,
+    rowMode: 'array'
+  })
+
+  client.release()
+
+  return rows
+}
+
 async function update_candidato (id, nuevo_nombre, nuevo_foto) {
   const client = await pool.connect()
 
@@ -44,17 +58,51 @@ async function update_candidato (id, nuevo_nombre, nuevo_foto) {
   client.release()
 }
 
-async function delete_candidato (id) {
+async function delete_candidato (id_str) {
+  const id = parseInt(id_str)
+
   const client = await pool.connect()
 
   await client.query({
+    text: 'delete from historial where ganador_id=$1',
+    values: [id]
+  })  
+
+  await client.query({
     text: 'delete from candidatos where id=$1',
-    values: [parseInt(id)]
+    values: [id]
   })
 
   client.release()
 }
 
+async function add_votos (estado, votos_str, nombre_ganador) {
+  const client = await pool.connect()
+
+  const { rows } = await client.query({
+    text: 'select * from candidatos where nombre=$1',
+    values: [nombre_ganador]
+  })
+
+  const ganador = rows[0]
+  const votos = parseInt(votos_str)
+
+  await client.query({
+    text: 'insert into historial (estado, votos, ganador_id) values ($1, $2, $3)',
+    values: [estado, votos, ganador.id]
+  })
+
+  await client.query({
+    text: 'update candidatos set votos=$1 where id=$2',
+    values: [ganador.votos + votos, ganador.id]
+  })
+
+  console.log(ganador);
+
+  client.release()
+}
+
 module.exports = {
-  add_candidato, get_candidatos, update_candidato, delete_candidato
+  add_candidato, get_candidatos, update_candidato, delete_candidato,
+  add_votos, get_historial
 }
--- elecciones/server.js
+++ elecciones/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {
-  add_candidato, get_candidatos, update_candidato, delete_candidato
+  add_candidato, get_candidatos, update_candidato, delete_candidato,
+  add_votos, get_historial
 } = require('./db.js')
 
 const app = express()
@@ -13,8 +14,10 @@ app.get('/candidatos', async (req, res) => {
   res.json(candidatos)
 });
 
-app.get('/historial', (req, res) => {
-  res.json([])
+app.get('/historial', async (req, res) => {
+  const historial = await get_historial()
+  console.log(historial);
+  res.json(historial)
 });
 
 app.post('/candidato', async (req, res) => {
@@ -24,8 +27,15 @@ app.post('/candidato', async (req, res) => {
 
   req.on('end', async () => {
     body = JSON.parse(body)
-
-    await add_candidato(body.nombre, body.foto, body.color)
+    try {
+      await add_candidato(body.nombre, body.foto, body.color)
+    } catch (error) {
+      // acá gestiono los errores
+      console.log(error);
+      if (error.code == '23505') {
+        return res.status(400).send({mensaje: 'este nombre de candidato ya existe'})
+      }
+    }
     
     res.json({todo: 'ok'})
   })
@@ -48,6 +58,20 @@ app.put('/candidato', async (req, res) => {
 app.delete('/candidato', async (req, res) => {
   await delete_candidato(req.query.id)
   res.json({todo: 'ok'})
+});
+
+app.post('/votos', async (req, res) => {
+  let body = ''
+
+  req.on('data', data => body += data)
+
+  req.on('end', async () => {
+    body = JSON.parse(body)
+
+    await add_votos(body.estado, body.votos, body.ganador)
+
+    res.json({todo: 'ok'})
+  })
 });
 
 app.listen(3000, () => console.log('Servidor ejecutando en puerto 3000'))
